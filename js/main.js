4d24b68b108f3f8491335fa30f8e73c213d6fd64
Nicholas Dunnaway <nicholas.dunnaway@coxautoinc.com>
2018-03-13 15:03:57 -0700

    W10 - Posted jQuery to plain JavaScript.

--- js/main.js
+++ js/main.js
@@ -1,56 +1,71 @@
-$(function() {
-    $(".playlist li").on("click", function() {
-        var curPlaying = $(".playing");
-        ClearPlaying();
+let videoPlayer = new VideoPlayer();
+let lastVideo = {};
 
-        var nextPlaying = $(this);
-        nextPlaying.addClass("playing");
-
-        $("#videoarea").attr({
-            "src": "vid/" + nextPlaying.attr("movieurl"),
-            "autoplay": "autoplay"
-        });
-        ScrollToTop();
-    });
-
-    // Load first video on page.
-    var curVideo = $(".playlist li").eq(0);
-    curVideo.addClass("playing");
-    $("#videoarea").attr({
-        "src": "vid/" + curVideo.attr("movieurl")
-    });
-});
-
-$(function() {
-    $("#videoarea").bind('ended', function() {
-        var curPlaying = $(".playing");
-        var lastVideo = curPlaying.attr("lastvideo");
-
-        if (lastVideo != "true")
-        {
-            ClearPlaying();
-
-            var nextPlaying = curPlaying.next();
-            nextPlaying.addClass("playing");
-
-            var lastVideo = curPlaying.attr("lastvideo");
-
-            $("#videoarea").attr({
-                "src": "vid/" + nextPlaying.attr("movieurl"),
-                "autoplay": "autoplay"
-            });
-        }
-    });
-});
-
-function ClearPlaying()
-{
-    $(".playing").removeClass("playing");
+/**
+ * Fire on page load.
+ */
+function OnLoad() {
+  console.log("loaded");
+  let elm = document.getElementById("firstVideo");
+  lnkChangeVideo(elm, true);
 }
 
-function ScrollToTop()
-{
-    $('html, body').animate({
-        scrollTop: $("#top").offset().top
-    }, 200)
+/**
+ * Change the playing video
+ * @param sender
+ * @param pause
+ */
+function lnkChangeVideo(sender, pause) {
+  console.log("Playing " + sender.getAttribute("movieurl"));
+  videoPlayer.SetSource("vid/" + sender.getAttribute("movieurl"));
+  if (!pause) {
+    videoPlayer.Play();
+  }
+  ClearPlaying();
+  SetPlaying(sender);
+  ScrollToTop();
+}
+
+/**
+ * Video end handler
+ */
+function OnVideoEnd(){
+  if(lastVideo.getAttribute("lastvideo")) {
+    console.log("End of playlist.");
+    return;
+  }
+  let nextPlaying = lastVideo.nextElementSibling;
+  lnkChangeVideo(nextPlaying);
+}
+
+/**
+ * Sets the playing video class to playing.
+ */
+function SetPlaying(elm) {
+  elm.classList.add('playing');
+  lastVideo = elm;
+}
+
+/**
+ * Clears the playing class.
+ */
+function ClearPlaying() {
+  let playing = document.getElementsByClassName("playing");
+  if(playing.length > 0)
+  {
+    playing[0].classList.remove("playing");
+  }
+}
+
+/**
+ * Scrolls to the top of the page.
+ * Source: https://stackoverflow.com/questions/1144805/scroll-to-the-top-of-the-page-using-javascript-jquery
+ * Credit to: wake-up-neo 05/29/2015
+ */
+function ScrollToTop() {
+  let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (currentScroll > 0) {
+    window.requestAnimationFrame(ScrollToTop);
+    window.scrollTo (0,currentScroll - (currentScroll/5));
+  }
 }
---
+++ js/video.js
@@ -0,0 +1,97 @@
+/**
+ * Video Player Object
+ */
+function VideoPlayer() {
+  let self = this;
+  self.view = null;
+  self.VideoPlayerID = "videoPlayer";
+
+  /**
+   * Gets the Video Player DOM node
+   * @returns {null|*}
+   */
+  self.GetElement = function () {
+    if (self.view == null) {
+      self.view = document.getElementById(self.VideoPlayerID);
+    }
+    return self.view;
+  };
+
+  /**
+   * Play Video
+   */
+  self.Play = function () {
+    player = self.GetElement();
+    player.play();
+  };
+
+  /**
+   * Pause Video
+   */
+  self.Pause = function () {
+    player = self.GetElement();
+    player.pause();
+  };
+
+  /**
+   * Vol Up
+   */
+  self.VolUp = function () {
+    player = self.GetElement();
+    player.volume += 0.2;
+  };
+
+  /**
+   * Vol Down
+   */
+  self.VolDown = function () {
+    player = self.GetElement();
+    player.volume -= 0.2;
+  };
+
+  /**
+   * Vol Mute
+   */
+  self.VolMute = function () {
+    player = self.GetElement();
+    player.muted = player.muted !== true;
+  };
+
+  /**
+   * Rewind
+   */
+  self.Rewind = function () {
+    player = self.GetElement();
+    if(player.currentTime - 5 < 0)
+    {
+      player.currentTime = 0;
+    }
+    else
+    {
+      player.currentTime -= 5;
+    }
+  };
+
+  /**
+   * Skip
+   */
+  self.Skip = function () {
+    player = self.GetElement();
+    if(player.currentTime + 5 > player.duration)
+    {
+      player.currentTime = player.duration;
+    }
+    else
+    {
+      player.currentTime += 5;
+    }
+  };
+
+  /**
+   * Set Source
+   */
+  self.SetSource = function (value) {
+    player = self.GetElement();
+    player.src = value;
+  };
+}
